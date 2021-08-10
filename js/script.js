94eda9bc34d57f4a2c779f15bec8ad335487ddf9
Ilyas <ilyas.sultanov2020@list.ru>
2021-08-10 06:03:50 +0500

    clean code

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-// Other 
 let inputBox = document.createElement('input');
 let pointBtn = document.getElementById('pointBtn');
 let clearBtn = document.getElementById('clearBtn');
-//Ariphmetic
 let equalsBtn = document.getElementById('equalsBtn');
 let plusBtn = document.getElementById('plusBtn');
 let minusBtn = document.getElementById('minusBtn');
 let timeBtn = document.getElementById('timeBtn');
 let divideBtn = document.getElementById('divideBtn');
-//Numbers
 let nineBtn = document.getElementById('nineBtn');
 let eightBtn = document.getElementById('eightBtn');
 let sevenBtn = document.getElementById('sevenBtn');
@@ -24,12 +21,11 @@ let inputSection = document.getElementById('inputSection');
 let numbersHeader
 let symbolsHeader;
 
-
-
 let currentNum1;
 let currentNum2;
 let symb;
-init()
+
+init();
 
 function init() {
     currentNum1 = '';
@@ -45,10 +41,8 @@ function init() {
 
     numbersHeader.innerText = '';
     symbolsHeader.innerText = '';
-
 }
 
-// Buttons
 nineBtn.addEventListener('click', () => { nine() });
 eightBtn.addEventListener('click', () => { eight() });
 sevenBtn.addEventListener('click', () => { seven() });
@@ -59,57 +53,29 @@ threeBtn.addEventListener('click', () => { three() });
 twoBtn.addEventListener('click', () => { two() });
 oneBtn.addEventListener('click', () => { one() });
 zeroBtn.addEventListener('click', () => { zero() });
-
-
 pointBtn.addEventListener('click', () => { dot() });
 
-function dot() {
-    dotBtn = '.';
-    inputBox.value += dotBtn;
-}
+function dot() { inputBox.value += '.'; }
 
+function zero() { inputBox.value += 0; }
 
-function zero() {
-    inputBox.value += 0;
-}
+function one() { inputBox.value += 1; }
 
-function one() {
-    inputBox.value += 1;
-}
+function two() { inputBox.value += 2; }
 
-function two() {
-    inputBox.value += 2;
-}
+function three() { inputBox.value += 3; }
 
-function three() {
-    inputBox.value += 3;
-}
+function four() { inputBox.value += 4; }
 
-function four() {
-    inputBox.value += 4;
-}
+function five() { inputBox.value += 5; }
 
-function five() {
-    inputBox.value += 5;
-}
+function six() { inputBox.value += 6; }
 
-function six() {
-    inputBox.value += 6;
-}
+function seven() { inputBox.value += 7; }
 
-function seven() {
-    inputBox.value += 7;
-}
+function eight() { inputBox.value += 8; }
 
-function eight() {
-    inputBox.value += 8;
-}
-
-function nine() {
-    inputBox.value += 9;
-}
-
-// Other
+function nine() { inputBox.value += 9; }
 
 clearBtn.addEventListener('click', () => { clear() });
 
@@ -117,7 +83,6 @@ function clear() {
     numbersHeader.innerText = '';
     symbolsHeader.innerText = '';
     inputBox.value = '';
-
 }
 
 plusBtn.addEventListener('click', () => {
@@ -125,36 +90,28 @@ plusBtn.addEventListener('click', () => {
     firstNum(symb);
 });
 
-function plus(num1, num2) {
-    return num1 + num2;
-}
+function plus(num1, num2) { return num1 + num2; }
 
 minusBtn.addEventListener('click', () => {
     symb = '-';
     firstNum(symb);
 });
 
-function minus(num1, num2) {
-    return num1 - num2;
-}
+function minus(num1, num2) { return num1 - num2; }
 
 timeBtn.addEventListener('click', () => {
     symb = '*';
     firstNum(symb);
 });
 
-function multiple(num1, num2) {
-    return num1 * num2;
-}
+function multiple(num1, num2) { return num1 * num2; }
 
 divideBtn.addEventListener('click', () => {
     symb = '/';
     firstNum(symb);
 });
 
-function divide(num1, num2) {
-    return num1 / num2;
-}
+function divide(num1, num2) { return num1 / num2; }
 
 equalsBtn.addEventListener('click', () => {
     resultDisplay();
@@ -167,7 +124,6 @@ equalsBtn.addEventListener('click', () => {
     } else if (symb == '/') {
         numbersHeader.innerText = divide(currentNum1, currentNum2);
     }
-
 });
 
 function firstNum(symbol) {
@@ -179,7 +135,6 @@ function firstNum(symbol) {
 }
 
 function resultDisplay() {
-
     currentNum2 = inputBox.value;
     currentNum2 = parseFloat(currentNum2);
     numbersHeader.style.fontSize = '50px';
